46c4fb118253739804f55c9177c71100691a33cb
Wesley Shih <shih.wesley@gmail.com>
2019-05-13 13:20:04 -0700

    Add files via upload
    Full-Stack folder

---
+++ pre-work/js.js
@@ -0,0 +1,15 @@
+function b1() {
+    document.getElementById("box").style.height = "250px";
+}
+
+function b2() {
+    document.getElementById("box").style.backgroundColor = "blue";
+}
+
+function b3() {
+    document.getElementById("box").style.opacity = "0.5";
+}
+
+function b4() {
+    document.getElementById("box").setAttribute("style", "height:150px; width:150px; opacity = 1.0; background-color:orange; margin:25px");
+}
